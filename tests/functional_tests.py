62c1b7db9e459fdacb6fdfcbec9a2faf53b4097a
npklein <niekdeklein@gmail.com>
2016-12-26 22:48:33 +0100

    fixed test importing

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,4 +1,4 @@
-from .context import genotypePublicData
+from context import genotypePublicData
 import requests
 
 # Get the Homo Sapien samples (tax 9606). Change for samples of different organism
@@ -6,15 +6,14 @@ tax = '9606'
 # Get the RNA-seq samples
 library_strategy = 'RNA-seq'
 
-
-url = 'http://www.ebi.ac.uk/ena/data/warehouse/search?query=%22tax_eq('+tax+')%20AND%20library_strategy=%22'+library_strategy+'%22%22&domain=read'
+download_ena_samples = genotypePublicData.Download_ENA_samplesheet(tax, library_strategy)
 
 # Dr. Gene Tica wants to get genotypes for all the latest RNAseq samples available on ENA (http://www.ebi.ac.uk/ena/)
 # To do this Gene needs to be able to download the FastQ files for all samples currently available
-response = requests.get(url)
+download_ena_samples.download_samplesheet('ENA')
 
 # The information Gene gets includes the link to the fastq file to download and the md5sum for checking validity of Fastq file
 # along with several other fields
-
+ass
 
 # Using the link to the fastq file the files are automatically downloaded to the provided outfolder
