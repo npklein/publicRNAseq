97e3eea0f8c229ddaf318a9ad268bfbe404af3b1
npklein <niekdeklein@gmail.com>
2016-12-27 13:38:23 +0100

    download file and change it to have datetime of current date and time

--- genotypePublicData/Download_ENA_samplesheet.py
+++ genotypePublicData/Download_ENA_samplesheet.py
@@ -3,6 +3,8 @@ import sys
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 from selenium import webdriver
 import time
+import os
+import shutil
 
 class Download_ENA_samplesheet:
     def __init__(self,tax_id='9606', library_strategy='RNA-seq'):
@@ -30,18 +32,60 @@ class Download_ENA_samplesheet:
             if x==360:
                 raise RuntimeError('Waited for page to load correctly '+str(360*sleep)+' seconds, did not find the text '+str(text))
         return html_source
+    
+    def __fully_downloaded(self, file_path, sleep=5):
+        '''Check if a file has fully downloaded by checking between intervals if file size has changed
         
+           file_path(str)   path of file to check if it's finished downloading
+           sleep(int)    interval time for checking if file size changed
+        '''
+        time.sleep(2)
+        logging.info('Checking if a .part file is used: '+file_path+'.part')
+        # some filesystems first download a .part file, check if this happens
+        if os.path.exists(file_path+'.part'):
+            logging.info('Check if '+str(file_path)+'.part finished downloading')
+            time.sleep(sleep)
+            while os.path.exists(file_path+'.part'):
+                logging.info('Still downloading, sleep '+str(sleep)+' seconds...')
+                time.sleep(sleep)
+            logging.info(file_path+'.part does not exist anymore, download should be finished')
+            assert os.path.exists(file_path)
+        else:
+            # if .part is not used, we need to wait to see if the file sizes are the same between intervals
+            old_size = os.stat(file_path).st_size
+            time.sleep(sleep)
+            new_size = os.stat(file_path).st_size
+            while new_size > old_size:
+                old_size = new_size
+                logging.info('Still downloading, sleep '+str(sleep)+' seconds...')
+                time.sleep(sleep)
+                new_size = os.stat(file_path).st_size
+            logging.info('File size did not change the last '+str(sleep)+' seconds, file should be done downloading')
+            assert new_size == old_size
+            if new_size < old_size:
+                raise RuntimeError('New size of downloaded file should never be larger than old size')
+        return True
+        
+    
+    def __prevent_download_dialog(self, output_directory):
+        '''Set Firefox profile such that it automatically downloads txt files to output_directory
+        
+           output_directory(str)    Directory to download files to
+        '''
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("browser.download.panel.shown", False)
+        profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/plain")
+        profile.set_preference("browser.download.folderList", 2);
+        profile.set_preference("browser.download.dir", output_directory)
+        return profile
+           
     def download_samplesheet(self, output_directory, tax_id='9606',library_strategy='RNA-Seq'):
         '''Download samplesheet from ENA
         output_file(str)    output file name for samplesheet    
         '''
         # To prevent download dialog
-        profile = webdriver.FirefoxProfile()
-        profile.set_preference("browser.download.panel.shown", False)
-        profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/plain")
-        profile.set_preference("browser.download.folderList", 2);
-    
-        self.driver = webdriver.Firefox(profile)
+
+        self.driver = webdriver.Firefox(self.__prevent_download_dialog(output_directory))
         logging.info('Downloading samplesheet for tax_id: '+self.tax_id+' and library strategy: '+self.library_strategy)
         url = 'http://www.ebi.ac.uk/ena/data/warehouse/search?query=%22tax_eq%289606%29%20AND%20library_strategy=%22RNA-Seq%22%22&domain=read'
         logging.info('Using url: '+url)
@@ -57,11 +101,17 @@ class Download_ENA_samplesheet:
             element.click()
             html_source = self.__page_loaded('>TEXT<')
             element = self.driver.find_element_by_xpath('//*[@title="Download files and save to disk" and contains(text(), "TEXT")]')
+            logging.info('Downloading to '+output_directory+'/ena.txt')
             element.click()
-            time.sleep(100)
-            self.quit()
+            if self.__fully_downloaded(output_directory+'/ena.txt'):
+                current_datetime = time.strftime("d%dm%my%Y_h%Hm%Ms%S")
+                logging.info('Renaming '+output_directory+'/ena.txt to '+output_directory+'/ena_'+current_datetime+'.txt')
+                shutil.move(output_directory+'/ena.txt', output_directory+'/ena_'+current_datetime+'.txt')
+            else:
+                raise RuntimeError('__fully_downloaded() did not return True')
+            self.quit_browser()
         except:
-            self.quit()
+            self.quit_browser()
             raise
         
     def set_tax_id(self,taxid):
@@ -70,5 +120,5 @@ class Download_ENA_samplesheet:
     def set_library_strategy(self,library_strategy):
         self.library_strategy = library_strategy
         
-    def quit(self):
+    def quit_browser(self):
         self.driver.quit()
